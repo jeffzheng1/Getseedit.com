fee5564d1e0df237645c2e415c828c7b88367bcf
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-02-16 17:35:20 -0800

    fixed resizing error, added navbar back

--- js/grayscale.js
+++ js/grayscale.js
@@ -4,6 +4,27 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-custom a").css("color", "#009933");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        if ($( window ).width() > 767) {
+            $(".navbar-custom a").css("color", "#fff");
+        }
+    }
+});
+
+$(window).resize(function() {
+    if ($( window ).width() > 767 && $(".navbar").offset().top < 50) {
+        $(".navbar-custom a").css("color", "#fff");
+    } else { 
+        $(".navbar-custom a").css("color", "#009933");
+    }
+});
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
@@ -13,4 +34,9 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
 });
