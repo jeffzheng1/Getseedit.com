f12fc67b507a5d40a256ee0c9589f5f76d26a569
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-02-19 01:00:13 -0800

    added sendgrid

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,10 +4,21 @@ require 'vendor/autoload.php';
 $sendgrid = new SendGrid("app34063474@heroku.com", "5Jc6%pk6.");
 $email    = new SendGrid\Email();
 
+if(empty($_POST['name'])        ||
+   empty($_POST['email'])       ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+    echo "No arguments Provided!";
+    return false;
+   }
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+
 $email->addTo("jeff.zheng.6@gmail.com")
       ->setFrom("you@youremail.com")
-      ->setSubject("Sending with SendGrid is Fun")
-      ->setHtml("and easy to do anywhere, even with PHP");
+      ->setSubject("Website Contact Form:  $name")
+      ->setHtml("You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address");
 
 $sendgrid->send($email);
 return true;			
