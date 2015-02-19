4021289f57219ee3e00055f9844b0cfc35448eea
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-02-19 00:49:24 -0800

    added sendgrid

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,23 +1,14 @@
 <?php
 require 'vendor/autoload.php';
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-	
-// Create the email and send the message
-$to = 'jeff.zheng.6@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+$sendgrid = new SendGrid("app34063474@heroku.com", "5Jc6%pk6.");
+$email    = new SendGrid\Email();
+
+$email->addTo("test@sendgrid.com")
+      ->setFrom("you@youremail.com")
+      ->setSubject("Sending with SendGrid is Fun")
+      ->setHtml("and easy to do anywhere, even with PHP");
+
+$sendgrid->send($email);
 return true;			
 ?>
