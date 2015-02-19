b86940419e5965066f29a2ab555a681e14b170c0
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-02-19 00:56:50 -0800

    added sendgrid

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 $sendgrid = new SendGrid("app34063474@heroku.com", "5Jc6%pk6.");
 $email    = new SendGrid\Email();
 
-$email->addTo("test@sendgrid.com")
+$email->addTo("jeff.zheng.6@gmail.com")
       ->setFrom("you@youremail.com")
       ->setSubject("Sending with SendGrid is Fun")
       ->setHtml("and easy to do anywhere, even with PHP");
