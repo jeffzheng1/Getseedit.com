3553b1e8a3cd18ac6b5362fd0eb3a351b02468e6
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-02-24 20:51:27 -0800

    added curl script

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,16 +1,43 @@
 <?php
-require '../vendor/autoload.php';
 // Check for empty fields
-$sendgrid = new SendGrid("app34063474@heroku.com", "5Jc6%pk6.");
-$email    = new SendGrid\Email();
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 
-$email->addTo("jeff.zheng.6@gmail.com")
-      ->setFrom("jeff.zheng.6@gmail.com")
-      ->setSubject("Website Contact Form:    $name    $email_address");
+$url = 'https://api.sendgrid.com/';
+$user = 'app34063474@heroku.com';
+$pass = '5Jc6%pk6.';
 
-$sendgrid->send($email);
+$params = array(
+    'api_user'  => $user,
+    'api_key'   => $pass,
+    'to'        => 'jeff.zheng.6@gmail.com',
+    'subject'   => '$name $email_address',
+    'html'      => 'testing body',
+    'text'      => 'testing body',
+    'from'      => 'example@sendgrid.com',
+  );
+
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
 return true;			
 ?>
