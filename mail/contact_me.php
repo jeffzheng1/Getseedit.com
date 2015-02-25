b03d51eb89f0a88df729c598366cece1c18c6fd7
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-02-24 20:45:22 -0800

    added preloader

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,20 +4,12 @@ require '../vendor/autoload.php';
 $sendgrid = new SendGrid("app34063474@heroku.com", "5Jc6%pk6.");
 $email    = new SendGrid\Email();
 
-if(empty($_POST['name'])        ||
-   empty($_POST['email'])       ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-    echo "No arguments Provided!";
-    return false;
-   }
-
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 
 $email->addTo("jeff.zheng.6@gmail.com")
-      ->setFrom("you@youremail.com")
-      ->setSubject("Website Contact Form:  $name    $email_address");
+      ->setFrom("jeff.zheng.6@gmail.com")
+      ->setSubject("Website Contact Form:    $name    $email_address");
 
 $sendgrid->send($email);
 return true;			
